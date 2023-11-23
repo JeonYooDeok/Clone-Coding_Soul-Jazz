bf864a646ec65e634f31f11c99b4ed68fc3a9571
JeonYooDeok <jyd910215@gmail.com>
2023-11-23 23:10:07 +0900

    클론코딩

---
+++ main.js
@@ -0,0 +1,8 @@
+const soulJazzRecordsSwiper = new Swiper('.soulJazzRecordsBookTitles.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
