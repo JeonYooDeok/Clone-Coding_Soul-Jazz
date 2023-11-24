c3494593f59490cbdd9d283a84ff397fbaac8cbb
JeonYooDeok <jyd910215@gmail.com>
2023-11-24 15:33:43 +0900

    featured 슬라이드 기능 추가

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const featuredSwiper = new Swiper('.featuredNews.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
+
 const soulJazzRecordsSwiper = new Swiper('.soulJazzRecordsBookTitles.swiper', {
   slidesPerView: 1,
   spaceBetween: 40,
